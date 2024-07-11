675d70c4679bc5844cfacd383badd46d12e1029c
Daniele Rossi <reds6188@gmail.com>
2024-07-11 15:01:53 +0200

    Added ANSI escape code for colors, added basic functions

--- include/console.h
+++ include/console.h
@@ -5,11 +5,28 @@
 
 #define TAG_MAX_LENGTH		7
 
+// ANSI colors ------------------------
+#define LOG_NORM        "\033[0m"
+#define LOG_RED         "\033[1;31m"
+#define LOG_GREN        "\033[1;32m"
+#define LOG_BLUE        "\033[1;34m"
+#define LOG_YELW        "\033[1;33m"
+#define LOG_MAGN        "\033[1;35m"
+#define LOG_CYAN        "\033[1;36m"
+#define LOG_ORNG        "\033[38;5;214m"
+#define LOG_PURP        "\033[38;5;91m"
+
 class Console : public HardwareSerial {
 	private:
 		uint8_t _max_tag_length;
 	public:
 		Console(void);
+		void writeTag(const char * tag);
+		void log(const char * tag, String message);
+		void success(const char * tag, String message);
+        void info(const char * tag, String message);
+        void warning(const char * tag, String message);
+        void error(const char * tag, String message);
 };
 
 extern Console console;
--- src/console.cpp
+++ src/console.cpp
@@ -6,4 +6,42 @@ Console::Console(void) : HardwareSerial(0) {
 	_max_tag_length = TAG_MAX_LENGTH;
 }
 
+void Console::writeTag(const char * tag) {
+	char pad_str[_max_tag_length + 6];
+	strcpy(pad_str, "[ ");
+	memset(pad_str + 2, ' ', _max_tag_length);
+	strcpy(pad_str + 2 + _max_tag_length, " ] ");
+	strncpy(pad_str + 2, tag, strlen(tag));
+	print(pad_str);
+}
+
+void Console::log(const char * tag, String message) {
+	writeTag(tag);
+	println(message);
+}
+
+void Console::success(const char * tag, String message) {
+	print(F(LOG_GREN));
+	log(tag, message);
+	print(F(LOG_NORM));
+}
+
+void Console::info(const char * tag, String message) {
+	print(F(LOG_BLUE));
+	log(tag, message);
+	print(F(LOG_NORM));
+}
+
+void Console::warning(const char * tag, String message) {
+	print(F(LOG_YELW));
+	log(tag, message);
+	print(F(LOG_NORM));
+}
+
+void Console::error(const char * tag, String message) {
+	print(F(LOG_RED));
+	log(tag, message);
+	print(F(LOG_NORM));
+}
+
 Console console;
